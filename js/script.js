6ee03afebc420e4a3384812154bfbb0200e6b6af
Aman Chopra <amanchopra954@gmail.com>
2018-04-09 17:02:09 +0530

    removeEverything()

--- js/script.js
+++ js/script.js
@@ -97,16 +97,8 @@ function loadCart(){
     }
 }
 
-function removeItemFromCart(name) {
-    for (let i in cart) {
-        if (cart[i].name === name){
-            cart[i].count--;
-            if (cart[i].count == 0){
-                cart.splice(i, 1);
-            }
-            break;
-        }
-    }
+function removeEverything() {
+    cart.empty();
     saveCart();
     refreshCart();
 }
